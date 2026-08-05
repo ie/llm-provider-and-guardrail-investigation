00721273c5c646ec262f832715bb087899f014af
Jo Chong <jo.chung@toyota.com.au>
2026-08-05 10:25:11 +1000

    Use and import lexus styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import.meta.glob(
+    '../node_modules/@tmca/lexus-dls/**/*.css',
+    { eager: true },
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
